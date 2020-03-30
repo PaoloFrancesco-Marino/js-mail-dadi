d0cbe3dd6defa756fa7234c127a0505fb8e5aea2
Paolo Francesco Marino <paolo.francesco.marino@gmail.com>
2020-03-30 14:39:37 +0200

    add email search js

--- script/main.js
+++ script/main.js
@@ -1,1 +1,31 @@
-console.log('js work');
+/**
+ * Cerca in un Array
+ */
+
+// 1. chiedi all'utente la sua email
+
+var email = ['michele@boolean.careers', 'fabio@boolean.careers', 'roberto@boolean.careers'];
+console.table(email); 
+
+var emailUser = prompt('Inserisci la tua Email').toLowerCase();
+
+// 2. controlla che sia nella lista di chi può accedere
+
+var foundEmail = false;
+
+for (var i = 0; i < email.length; i++) {
+    var user = email[i];
+
+    if (emailUser == user) {
+        foundEmail = true;
+    }
+}
+
+// 3. stampa un msg appropriato sull'esito del controllo
+
+if (foundEmail == true) {
+    // utente trovato
+    console.log('email trovata');
+} else {
+    console.log('email non trovata');
+}
