517ea9948329f226ca663a85ca0daa020bc33475
Paolo Francesco Marino <paolo.francesco.marino@gmail.com>
2020-03-30 18:07:08 +0200

    add Initial style css

---
+++ script/email.js
@@ -0,0 +1,61 @@
+/**
+ * Cerca in un Array
+ */
+
+// array di dati
+
+var email = ['michele@boolean.careers', 'fabio@boolean.careers', 'roberto@boolean.careers'];
+console.table(email); 
+
+// 1. chiedi all'utente la sua email
+
+var nome = document.getElementById('email').value;
+var login = document.getElementById('login');
+var message;
+
+
+login.addEventListener('click',
+    function() {
+        var nome = document.getElementById('email').value;
+
+        var foundEmail = false;
+
+        for (var i = 0; i < email.length; i++) {
+            var user = email[i];
+        
+            if (nome == user) {
+                foundEmail = true;
+            }
+        }
+
+        // 3. stampa esito del controllo
+
+        if (foundEmail == true) {
+            // utente trovato
+            console.log('email trovata');
+            message = 'Email trovata'
+        } else {
+            // utente non trovato
+            console.log('email non trovata');
+            message = 'Eamil non trovata'
+        }
+
+        // inserisco in html
+        document.getElementById('autenticazione') .innerHTML = message;
+    }
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
